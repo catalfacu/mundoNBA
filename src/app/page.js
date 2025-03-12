b3376ac1207ff3c9f13a6bf98cbd41e66b096365
Facundo Cataldo <cataldof21@gmail.com>
2025-03-11 22:58:36 -0300

    Recordando funciones y eventos

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,8 @@
+'use client'
+
 import Image from 'next/image'
+import { counter } from '@/utils/utils'
+
 
 export default function Home() {
   return (
@@ -6,9 +10,9 @@ export default function Home() {
       <h1  className='text-xl'>
         No me acuerdo de nada, inutil TOTAL!
       </h1>
-      <button>+</button>
+      <button value={'+'} onClick={(e)=> counter(e.target.value)}>+</button>
       <h1>Contador</h1>
-      <button>-</button>
+      <button value={'-'} onClick={(e)=> counter(e.target.value)}>-</button>
     </div>
   )
 }
---
+++ src/utils/utils.js
@@ -0,0 +1,9 @@
+
+
+export const counter = (value) =>{
+    if(value === '+') {
+        return console.log('sumando')
+    } else{
+        return console.log('restando')
+    }
+};
